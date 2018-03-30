38ccb1b1c00c002d2934cfa1475b0851c8545c68
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-03-30 16:26:10 +0000

    done some models

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,4 +4,24 @@ class Cart < ActiveRecord::Base
   has_many :line_items
   has_many :items, through: :line_items
 
+  def total
+    all_price = 0
+    self.line_items.each { |line_item|
+      all_price += line_item.item.price * line_item.quantity
+    }
+    all_price
+  end
+
+  def add_item(item_id)
+    if line_item = self.line_items.find { |line_item| line_item.item_id == item_id }
+      line_item.quantity += 1
+    else
+      line_item = LineItem.new(item_id: item_id, cart_id: self.id, quantity: 1)
+    end
+  end
+
+  def available_items
+    Item.where("inventory >= 1")
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,46 @@
 
 ActiveRecord::Schema.define(version: 20180330152439) do
 
+  create_table "carts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "status",     default: "not submitted"
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.float    "price"
+    t.string   "inventory"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer  "item_id"
+    t.integer  "cart_id"
+    t.integer  "quantity",   default: 1
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.integer  "user_id"
+    t.integer  "line_item_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
